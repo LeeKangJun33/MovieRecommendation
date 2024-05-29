ff18fb92b6b80b480b6cd881c4fc366a57b2c40d
LeeKangJun <drfting1230@naver.com>
2024-05-30 00:01:19 +0900

    init:쿠버 관련 추가

---
+++ src/main/java/com/example/movierecommendation/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.example.movierecommendation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    public RedisTemplate<?,?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
